b834819ce5bc7a6a955c094b7b37b2ba0bb5df79
Aayush Seth <aayushseth8@gmail.com>
2024-03-28 17:30:31 -0400

    Connect frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
+import axios from "axios";
+
+import { useState, useEffect } from "react";
 
 function App() {
+  const [embeddings, setEmbeddings] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/embeddings").then((response) => {
+      console.log("hello");
+      const res = response.data;
+      console.log(res);
+      setEmbeddings(res);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <p>Learn React</p>
+      <p>hello</p>
+
+      {Object.entries(embeddings).map(([key, value]) => (
+        <p>
+          {key}, {value}
+        </p>
+      ))}
     </div>
   );
 }
--- server/app.py
+++ server/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+from flask_cors import CORS, cross_origin
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def home():
     return "Hello, World!"
 
@@ -10,12 +14,14 @@ def home():
 dummy_embeddings = {
     'CNN': [0.1, 0.2, 0.3],
     'Fox News': [0.4, 0.5, 0.6],
-    'BBC': [0.7, 0.8, 0.9]
+    'BBC': [0.7, 0.8, 0.9],
+    'CNBC': [0.2, 0.8, 0.9]
 }
 
 @app.route('/embeddings')
+@cross_origin()
 def get_embeddings():
     return jsonify(dummy_embeddings)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
