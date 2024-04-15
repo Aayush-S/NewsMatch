e7419579e7e874253946ffc1ba39abab5b70b704
Aayush Seth <aayushseth8@gmail.com>
2024-04-15 14:40:33 -0400

    Create route

--- server/app.py
+++ server/app.py
@@ -1,9 +1,16 @@
+import sqlite3
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-from flask_cors import CORS, cross_origin
+
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 @cross_origin()
@@ -22,6 +29,20 @@ dummy_embeddings = {
 @cross_origin()
 def get_embeddings():
     return jsonify(dummy_embeddings)
+
+@app.route('/articles')
+@cross_origin()
+def get_articles():
+
+    conn = get_db_connection()
+    conn = conn.cursor()
+    conn.execute("SELECT * FROM articles LIMIT 10")
+    articles = conn.fetchall()
+    conn.close()
+
+    return jsonify([dict(article) for article in articles])
+
+    # return jsonify(dummy_embeddings)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
